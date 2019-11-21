dcda6c5cc3f3a560cf6ab55a843056934e7a29ce
Krystian Kościelniak <krystiankoscielniak@gmail.com>
2019-11-21 12:47:32 +0100

    Add alacritty config

--- setup-osx.sh
+++ setup-osx.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-# Install oh-my-zsh 
+# Install oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-echo "oh-my-zsh installed" 
+echo "oh-my-zsh installed"
 
-# Create .profile file (only at the first setup) 
+# Create .profile file (only at the first setup)
 # echo 'source ~/dotfiles/zshrc' >> ~/.zshrc
-# echo ".zshrc file updated" 
+# echo ".zshrc file updated"
 
 # Link config files
 rm ~/.gitconfig
 ln -s $PWD/gitconfig ~/.gitconfig
 echo ".gitconfig linked"
+
+rm ~/.gitignore
+ln -s $PWD/gitignore ~/.gitignore
+echo ".gitignore linked"
 
 rm ~/.vimrc
 ln -s $PWD/vimrc ~/.vimrc
@@ -19,15 +23,19 @@ echo ".vimrc linked"
 
 rm ~/.gvimrc
 ln -s $PWD/gvimrc ~/.gvimrc
-echo ".gvimrc linked" 
+echo ".gvimrc linked"
 
 rm ~/.yabairc
 ln -s $PWD/yabairc ~/.yabairc
-echo ".yabairc linked" 
+echo ".yabairc linked"
 
 rm ~/.skhdrc
 ln -s $PWD/skhdrc ~/.skhdrc
 echo ".skhdrc linked"
+
+rm -rf ~/.config/alacritty
+ln -s $PWD/alacritty.yml ~/.alacritty.yml
+echo ".alacritty.yml linked"
 
 # Allow VSCode to use my zsh config:
 cp zsh-login /usr/local/bin/zsh-login
@@ -36,7 +44,7 @@ echo "VSCode can use /usr/local/bin/zsh-login as integrated terminal"
 
 # Speed up Mission Control
 defaults write com.apple.dock expose-animation-duration -float 0.1
-echo "Mission Control speed increased" 
+echo "Mission Control speed increased"
 
 # Disable the “Are you sure you want to open this application?” dialog
 defaults write com.apple.LaunchServices LSQuarantine -bool false
